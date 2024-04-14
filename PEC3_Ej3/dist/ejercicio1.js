952a77925d6b2bae32b3f373261caea343fd725b
OmarHeviaArbana <ohevia@uoc.edu>
2024-04-14 12:21:13 +0200

    Correction for successful compilation of file exercise1.t and updated README.md

--- PEC3_Ej3/dist/ejercicio1.js
+++ PEC3_Ej3/dist/ejercicio1.js
@@ -1,7 +1,8 @@
+"use strict";
 function printArray(array) {
     console.log(array.slice(1).join(',')); // code to print the array on console
 }
-var array = [2, 3, 4];
+let array = [2, 3, 4];
 console.log(array[0]); //2
 printArray(array); // 3,4
 array.push(5);
@@ -14,9 +15,9 @@ printArray(array); // 3,4,1
 array.shift();
 array.unshift(8);
 printArray(array); // 8,3,4,1
-var everyisgreater = array.every(function (num) { return num > 3; }); //check if every number is greater than 3
+let everyisgreater = array.every(num => num > 3); //check if every number is greater than 3
 console.log(everyisgreater); //false
-var everyisless = array.every(function (num) { return num < 10; }); // check if every number is less than 10
+let everyisless = array.every(num => num < 10); // check if every number is less than 10
 console.log(everyisless); //true
 console.log(array.sort()); //1,3,4,8
 console.log(array.reverse()); //8,4,3,1
